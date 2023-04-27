8bedd1e5c330ab1af6913dd50923d0fc40905a3b
Davy Assunção <davydesouzabar@gmail.com>
2023-04-26 21:04:49 -0400

    refactor: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-htpp-client.spec.ts
+++ src/infra/http/axios-http-client/axios-htpp-client.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosHttpClient } from "./axios-http-client";
 import axios from "axios";
 import { faker } from "@faker-js/faker";
+import { HttpPostParams } from "@/data/protocols/http";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -9,13 +10,16 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.helpers.objectValue<any>({}),
+});
+
 describe("AxiosHttpClient", () => {
-  test("Should call axios with correct url and verb", async () => {
-    const url = faker.internet.url();
+  test("Should call axios with correct values", async () => {
     const sut = makeSut();
-    await sut.post({
-      url,
-    });
-    expect(mockedAxios.post).toBeCalledWith(url);
+    const response = mockPostRequest();
+    await sut.post(response);
+    expect(mockedAxios.post).toBeCalledWith(response.url, response.body);
   });
 });
